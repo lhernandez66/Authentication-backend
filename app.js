ebbee0642f4ed6ca4104dddfb6fe331c9c76e2fa
lhernandez66@ivytech.edu <lhernandez66@ivytech.edu>
2025-04-08 20:47:49 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//setup.. this is similar to when we use our default tags in html
+const express = require("express")
+//activate or tell this app variable to be an express server
+const app = express()
+const router = express.Router()
+
+//start the web server... app.listen(portnumber,function)
+app.listen(3000,function(){
+    console.log("Listening on port 3000")
+})
+
+//making an api using routes
+//routes are used to handle browser request. They look like urls. The difference is that when a browser requests a route, it is dynamically handled by using a function.
+
+//GET or a regular request when someone goes to http://localhost:3000/hello. When using a function in a route, we almost always have a parameter or handle a response and a request
+
+app.get("/hello", function(req,res){
+    res.send("<h1>Hello Express</h1>")
+})
+
+app.get("/goodbye", function(req,res){
+    res.send("<h1>Goodbye, Express!</h1>")
+})
