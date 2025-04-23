0dbcc8c25ab598b79b624da18b4ed1525019886c
lhernandez66@ivytech.edu <lhernandez66@ivytech.edu>
2025-04-22 22:00:30 -0400

    added db

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 // Setup – similar to default tags in HTML
 const express = require("express")
-// We have to use cors in order to host front end and backend on the same site
-var cors = require("cors")
+const Song = require("./models/songs")
+var cors = require('cors')
 
-// Activate or tell this app variable to be an express server
 const app = express()
 app.use(cors())
+// Middleware that parses HTTP requessts with JSON body
+app.use(express.json())
+
 const router = express.Router()
 
-// making an api using routes
-// routes are used to handle browser requests. They look like urls.
-router.get("/songs", function (req, res) {
-    const songs = [
-        {
-            title: "We Found Love",
-            artist: "Rihanna",
-            popularity: 10,
-            releaseDate: new Date(2011, 9, 22),
-            genre: ["electro house"]
-        },
-        {
-            title: "Happy",
-            artist: "Pharrell Williams",
-            popularity: 10,
-            releaseDate: new Date(2013, 11, 21),
-            genre: ["soul", "new soul"]
-        }
-    ]
-    res.json(songs)
-})
+//grab all the songs on a database
+router.get("/songs", async (req, res) => {
+    try {
+        const songs = await Song.find({});
+        res.send(songs);
+        console.log(songs);
+    } catch (err) {
+        console.log(err)
+    }
+});
+router.post("/songs", async (req, res) => {
+    try {
+        const song = new Song(req.body);
+        await song.save();
+        res.status(201).json(song);
+        console.log(song);
+    } catch (err) {
+        res.status(400).send(err);
+        console.log(err);
+    }
+});
+
 
 // All requests that usually use api start with /api... so the url would be localhost:3000/api/songs
 app.use("/api", router)
---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://lhernandez66:password255@songdb.u6ersye.mongodb.net/?retryWrites=true&w=majority&appName=SongDB")
+
+module.exports = mongoose
